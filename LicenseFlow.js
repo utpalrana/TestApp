aba69d8bcab79118e26b34870347ddff55efd08a
Utpal Rana <utpalrana@yahoo.com>
2025-05-06 14:01:45 +0530

    Add files via upload

---
+++ LicenseFlow.js
@@ -0,0 +1,27 @@
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script>
+$(document).ready(function () {
+    $('a[data-button-type="add-cart"]').on('click', function (e) {
+        e.preventDefault();
+
+        if (!window.Customer || !window.Customer.id) {
+            window.location.href = "/login.php";
+            return;
+        }
+
+        const productId = new URLSearchParams(this.href.split('?')[1]).get('product_id');
+        if (!productId) {
+            alert('Product ID missing!');
+            return;
+        }
+
+        $.post('/cart.php', {
+            action: 'add',
+            product_id: productId,
+            qty: 1
+        }, function () {
+            alert('✅ Product added to cart!');
+        });
+    });
+});
+</script>
